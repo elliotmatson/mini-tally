05998267dce823504ea7c90bed87fb4421d139bb
Elliot Matson <ematson5897@gmail.com>
2023-12-23 13:39:56 -0600

    mdns testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
+
 #include <AsyncTCP.h>
 #include <regex>
+#include <ESPmDNS.h>
+
 
 #include "helpers.h"
 #include "api.h"
@@ -24,7 +27,40 @@ void setup() {
   updateCallbacks(getLedState());
   ESP_LOGI(__func__, "Mini Tally setup complete.");
 
-  delay(5000);
+  // mdns discover
+  ESP_LOGI(__func__, "Discovering mDNS services...");
+  if (!MDNS.begin("mini-tally")) {
+    ESP_LOGE(__func__, "Error setting up mDNS responder!");
+  }
+  MDNS.setInstanceName("Mini Tally");
+  MDNS.addService("poop", "tcp", 80);
+  delay(1000);
+  mdns_result_t *results = NULL;
+  esp_err_t err = mdns_query_ptr("_http", "_tcp", 10000, 20, &results);
+  if (err) {
+    ESP_LOGE(__func__, "Query Failed");
+  }
+  ESP_LOGI(__func__, "Query Results:");
+  for (mdns_result_t *r = results; r; r = r->next) {
+    ESP_LOGI(__func__, "  %s (%s:%d)", r->instance_name, r->addr, r->port);
+  }
+  mdns_query_results_free(results);
+  ESP_LOGI(__func__, "mDNS setup complete.");
+
+
+
+  /*uint8_t serviceCount = MDNS.queryService("_http", "_tcp");
+  if (serviceCount == 0) {
+    ESP_LOGE(__func__, "No services found");
+  } else {
+    ESP_LOGI(__func__, "Found %d services", serviceCount);
+    for (int i = 0; i < serviceCount; ++i) {
+      // Print details for each service found
+      ESP_LOGI(__func__, "  %d: %s (%s:%d)", i + 1, MDNS.hostname(i).c_str(), MDNS.IP(i).toString().c_str(), MDNS.port(i));
+    }
+  }*/
+
+  delay(1000000);
   // setup Telnet connection to Streaming Bridge
   ESP_LOGI(__func__, "Connecting to Streaming Bridge...");
   AsyncClient *client = new AsyncClient();
@@ -110,6 +146,9 @@ void setup() {
     client->write("notify: transport: true\n");},
                     NULL);
   client2->connect(remoteIP2, 9993);
+
+  // setup USB ethernet driver
+  ESP_LOGI(__func__, "Setting up USB ethernet driver...");
 
   delay(1000000);
 }
